f057ab4a20b1158de13605bd69c0fe0783e6972c
safetychain <dthreatt@safetychain.com>
2025-03-24 12:57:55 -0500

    fixed url

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: 'https://derrickthreatt.com/sitemap.xml',
+    sitemap: 'https://www.gcphoatx.com/sitemap.xml',
   };
 }
